1b524fd7867c9db67b7d8515d0214d0a2d16fba4
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-05-22 22:15:45 +0530

    Update Parenthesis_checker.java
    Balanced Parenthesis Checker

--- Stacks/Parenthesis_checker.java
+++ Stacks/Parenthesis_checker.java
@@ -1,7 +1,26 @@
+// created by @shivam24-2000
 
-class main{
-  public static void main(String[] args)
+public static boolean matching(char a, char b)
+{
+  return ((a=='[' && b==']') || (a=='(' && b==')') || (a=='{' && b=='}') );
+}
+
+public static boolean Parenthesis( String str)
+{
+  Deque<Character> s = new ArrayDeque<>();
+  
+  for(int i=0;i<s.length();i++)
   {
-    
+    if(str.charAt(i) == '(' || str.charAt(i) == '[' || str.charAt(i) == '{') 
+      s.push(s.charAt(i);
+    else{
+      if(s.isEmpty() == true)
+        return false;
+      elseif( matching(s.peek(), str.charAt(i) ) == false)
+        return false;
+      else
+        s.pop();
+    }
   }
+  return (s.isEmpty() == true);
 }
