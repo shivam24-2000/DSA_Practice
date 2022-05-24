0530577dc84e45a51c4a824b3aa83e07a17929c6
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-05-24 16:18:38 +0530

    InfixToPostfix

---
+++ Stacks/InfixToPostfix.java
@@ -0,0 +1,50 @@
+class Solution
+{
+  static int prec(char c)
+  {
+    if( c == '^') return 3;
+    else if ( c == '*' || c == '/') return 2;
+    else if ( c == '+' || c == '-') return 1;
+    else return -1;
+  }
+  
+  public static String infixTopostfix( String s )
+  {
+    Stack<Character> st = new Stack<>();
+    String res = " ";
+    for(int i=0;i<s.length();i++)
+    {
+      char c = s.charAt(i);
+      
+      if( Character.isLetterOrDigit(c))
+        res += c;
+      else if ( c == '(')
+      {
+        st.push('(');
+      }
+      else if ( c == ')')
+      {
+        while( st.peek() != '(')
+          res += st.peek();
+          st.pop();
+        
+        st.pop();
+      }
+      else
+      {
+        while( !st.isEmpty() && prec(c) <= prec(st.peek())
+              {
+                res += st.peek();
+                st.pop();
+              }
+              st.push(c);
+      }
+    }
+    while( !st.isEmpty)
+              {
+                res += st.peek();
+                st.pop();
+              }
+    return res;
+  }
+}
