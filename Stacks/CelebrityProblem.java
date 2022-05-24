8753790d5f5cd21b86026ff1967d06c445bc079d
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-05-24 21:37:11 +0530

    :star:

---
+++ Stacks/CelebrityProblem.java
@@ -0,0 +1,16 @@
+int celebrity(M[][], int n)
+{
+  int a = 0, b = n-1;
+  
+  while( a < b)
+  {
+    if( M[a][b] == 1) a++;
+    else b--;
+  }
+  
+  for(int i=0;i<n;i++)
+  {
+    if( (i != a) && ( M[a][i] == 1 || M[i][a] == 0 ) ) return -1;
+  }
+  return a;
+}
