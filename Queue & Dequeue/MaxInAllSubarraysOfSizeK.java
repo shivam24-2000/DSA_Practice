f15f22fb67c520d92a728d161bd4c238aa1f37eb
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-05-26 16:56:04 +0530

    Max in Subarray of size k

---
+++ Queue & Dequeue/MaxInAllSubarraysOfSizeK.java
@@ -0,0 +1,29 @@
+void printMax( int arr[], in k)
+{
+  int n = arr.length;
+  
+  Deque<Integer> dq = new ArrayDeque<>();
+  
+  for(int i=0;i<k;i++)
+  {
+    while( !dq.isEmpty() && arr[i] <= arr[dq.peekLast()] )
+      dq.removeLast();
+    
+    dq.addLast(i);
+  }
+  for(int i=k; i<n; i++)
+  {
+    System.out.println(arr[dq.peek()] +" ");
+    
+    while( !dq.isEmpty() && dq.peek() <= i-k)
+      dq.removeLast(i);
+    
+    while( !dq.isEmpty() && arr[i] <= arr[dq.peekLast()] )
+      dq.removeLast(i);
+    
+    dq.addLast(i);
+    
+  }
+  System.out.println(arr[dq.peek()]);
+  
+}
