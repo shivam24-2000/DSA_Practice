4b8568475204f2017bdbc8ee0fb83aa3d06f7d5a
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-05-26 17:17:57 +0530

    :star:

--- Queue & Dequeue/MaxInAllSubarraysOfSizeK.java
+++ Queue & Dequeue/MaxInAllSubarraysOfSizeK.java
@@ -1,30 +1,38 @@
 
-void printMax( int arr[], in k)
+static ArrayList <Integer> max_of_subarrays(int arr[], int k)
 {
   int n = arr.length;
   
-  Deque<Integer> dq = new ArrayDeque<>();
+   Deque<Integer> dq = new ArrayDeque<Integer>();
   
-  for(int i=0;i<k;i++)
-  {
-    while( !dq.isEmpty() && arr[i] >= arr[dq.peekLast()] )
-      dq.removeLast();
-    
-    dq.addLast(i);
-  }
-  for(int i=k; i<n; i++)
-  {
-    System.out.println(arr[dq.peek()] +" ");
-    
-    while( !dq.isEmpty() && dq.peek() <= i-k)
-      dq.removeFirst(i);
-    
-    while( !dq.isEmpty() && arr[i] >= arr[dq.peekLast()] )
-      dq.removeLast(i);
-    
-    dq.addLast(i);
-    
-  }
-  System.out.println(arr[dq.peek()]);
+        for (int i = 0; i < k; ++i) {
+            
+            while (!dq.isEmpty() && arr[i] >= arr[dq.peekLast()])
+                dq.removeLast(); 
+
+            
+            dq.addLast(i);
+        }
+
+       ArrayList<Integer> list = new ArrayList<>();
+        for (int i=k; i < n; ++i) {
+            
+            list.add(arr[dq.peek()]);
+
+            
+            while ((!dq.isEmpty()) && dq.peek() <= i - k)
+                dq.removeFirst();
+
+           
+            while ((!dq.isEmpty()) && arr[i] >= arr[dq.peekLast()])
+                dq.removeLast();
+
+           
+            dq.addLast(i);
+        }
+
+        
+        list.add(arr[dq.peek()]);
+        return list;
   
 }
