384d1cb38b693c222ab236bde884571e29758882
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-05-27 19:15:04 +0530

    Update HeightOfTree.java

--- tree/HeightOfTree.java
+++ tree/HeightOfTree.java
@@ -2,5 +2,5 @@ void height(Node root)
 {
   if( root == null) return 0;
   else
-    return Math.max( (root.left), (root.right) ) +1;
+    return Math.max( height(root.left), height(root.right) ) +1;
 }
