65565dea3b64d702c282704b32e4d2d1990c77f9
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-06-03 15:51:25 +0530

    No of island :star: :star: :star:

---
+++ Graph/Island.java
@@ -0,0 +1,42 @@
+void BFS(ArrayList<ArrayList<Integer>> adj, int s, boolean[] visited)
+{
+  Queue<Integer> q = new LinkedList<>();
+  
+  visited[s] = true;
+  
+  q.add(s);
+  
+  while( !q.isEmpty)
+  {
+    int u = q.poll();
+    System.out.print(u);
+    
+    for(int  v : adj.get(u) )
+    {
+      if( visited[v] == false)
+      {
+        visited[v] = true;
+        q.add(v);
+      }
+    }
+  }
+}
+
+int BFSDis(ArrayList<ArrayList<Integer>> adj, int v)
+{
+  int count = 0;
+  boolean visited[v+1];
+  for(int i=0;i<v;i++)
+  {
+    visited[i] = false;
+  }
+  for(int i=0;i<v;i++)
+  { 
+    if(visited[i] == false)
+    {
+      BFS(adj, i, visited));
+      count++;
+    }
+  }
+  return count;
+}
