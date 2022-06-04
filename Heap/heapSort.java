50c2cf6265a4e8e16120d11c3048856d930d5bd6
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-06-04 15:08:23 +0530

    Heap Sort :star:

---
+++ Heap/heapSort.java
@@ -0,0 +1,39 @@
+public void buildheap(int arr[], int n)
+{
+  for(int i=(n-2)/2; i>=0;i--)
+    heapify(arr, n, i);
+}
+
+public void sort(int arr[])
+{
+  int n = arr.length;
+  buildheap(arr, n);
+  
+  for(int i=n-1;i>0;i--)
+  {
+    int temp = arr[0];
+    arr[0] = arr[i];
+    arr[i] = temp;
+    
+    heapify(arr, i, 0);
+  }
+}
+
+void heapify(int arr[], int n, int i)
+{
+  int largest = i;
+  int left = 2*i+1;
+  int right = 2*i+2;
+  
+  if(left < n && arr[left] > arr[largest]) largest = left;
+  if(right < n && arr[right] > arr[largest]) largest = right;
+  
+  if( largest != i)
+  {
+    int temp = arr[i];
+    arr[i] = arr[largest];
+    arr[largest] = temp;
+    
+    heapify(arr,n,largest);
+  }
+}
