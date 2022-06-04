0390f3a18eeba03b3060ca9680856c862c2089b4
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-06-04 15:15:56 +0530

    Heap Sort :star:

--- Heap/heapSort.java
+++ Heap/heapSort.java
@@ -1,39 +1,41 @@
-public void buildheap(int arr[], int n)
-{
-  for(int i=(n-2)/2; i>=0;i--)
-    heapify(arr, n, i);
-}
-
-public void sort(int arr[])
-{
-  int n = arr.length;
-  buildheap(arr, n);
-  
-  for(int i=n-1;i>0;i--)
-  {
-    int temp = arr[0];
-    arr[0] = arr[i];
-    arr[i] = temp;
+    void buildHeap(int arr[], int n)
+    {
+        // Your code here
+        for(int i= (n-2)/2; i>=0;i-- )
+            heapify(arr, n,i);
+    }
+ 
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i)
+    {
+        // Your code here
+        int largest = i;
+        int l = 2*i+1, r= 2*i+2;
+        
+        if(l<n && arr[l] > arr[largest]) largest = l;
+        if(r<n && arr[r] > arr[largest]) largest = r;
+        
+        if( largest != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
     
-    heapify(arr, i, 0);
-  }
-}
-
-void heapify(int arr[], int n, int i)
-{
-  int largest = i;
-  int left = 2*i+1;
-  int right = 2*i+2;
-  
-  if(left < n && arr[left] > arr[largest]) largest = left;
-  if(right < n && arr[right] > arr[largest]) largest = right;
-  
-  if( largest != i)
-  {
-    int temp = arr[i];
-    arr[i] = arr[largest];
-    arr[largest] = temp;
-    
-    heapify(arr,n,largest);
-  }
-}
+    //Function to sort an array using Heap Sort.
+    public void heapSort(int arr[], int n)
+    {
+        //code here
+        buildHeap(arr, n);
+        
+        for(int i=n-1;i>0;i--)
+        {
+            int temp  = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            
+            heapify(arr, i, 0);
+        }
+    }
