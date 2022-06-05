24fd68ec1e5246785f55713a5ed3e7aa833f2c0b
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-06-05 16:24:40 +0530

    Sorting based on frequency :star:

--- Hashing/SortingBasedOnFreq.java
+++ Hashing/SortingBasedOnFreq.java
@@ -1,50 +1,52 @@
-class solution
-{
-  static ArrayList<Integer> sortByFreq(int arr[], int n)
-  {
-    HashMap<Integer, Integer> hm = new HashMap<>();
-    
-    for(int i=0;i<n;i++)
+static ArrayList<Integer> sortByFreq(int arr[], int n)
     {
-      if( hm.containsKey(arr[i]) )
-        hm.put( arr[i], hm.get(arr[i]) +1);
-      else
-        hm.put(arr[i], 1);
+        // add your code here
+        Map<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            if( hm.containsKey(arr[i]) )
+            {
+                hm.put(arr[i], hm.get(arr[i]) +1);
+            }else
+            {
+                hm.put(arr[i], 1);
+            }
+        }
+        
+        // hm.forEach((k,v) -> System.out.println(k+v) );
+        Set<Entry<Integer, Integer>> entrySet = hm.entrySet();
+        
+        List<Entry<Integer, Integer>> list = new ArrayList<>(entrySet);
+        Collections.sort(list, new Comparator<Entry<Integer, Integer>>()
+        {
+            public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2)
+            {
+                if(o1.getValue() > o2.getValue())
+        			return -1;
+        		else if(o1.getValue() == o2.getValue()){
+        			if(o2.getKey() < o1.getKey())
+        			    return 1;
+        			else
+        			    return -1;
+		    
+        		}
+        		else if(o1.getValue() < o2.getValue())
+        			return 1;
+        		
+        		return Integer.MIN_VALUE;
+            }
+        });
+        ArrayList<Integer> asd = new ArrayList<>();
+        list.forEach( s->
+        {
+            int val  = s.getValue();
+            
+            for(int i=0;i<val;i++)
+            {
+                asd.add(s.getKey());
+            }
+        });
+        
+        return asd;
     }
-    
-    Set<Map.Entry<Integer, Integer>> entryset = hm.entrySet<>();
-    
-    List<Map.Entry<Integer, Integer>> list = new ArrayList<>(entryset);
-    
-    Collections.sort( list, new Comparator( Map.Entry<Integer, Integer>>()
-                                           {
-                                             public int compare( Map.Entry<Integer, Integer> o1,  Map.Entry<Integer, Integer> o2)
-                                             {
-                                               if( o1.getValue() > o2.getValue() )
-                                                 return -1;
-                                               else if (o1.getValue() == o2.getValue())
-                                               {
-                                                 if( o1.getValue() > o2.getValue() )
-                                                   return 1;
-                                                 else 
-                                                   return -1;
-                                               }
-                                               else if (o1.getValue() < o2.getValue())
-                                                 return 1;
-                                               
-                                               return Integer.MIN_VALUE;
-                                                 
-                                             }
-                                           });
-    ArrayList<Integer> Last1 = new ArrayList<Integer>();
-    list.forEach( s->
-                 {
-                   int val = s.getvalue();
-                   
-                   for(int i=0;i<val;i++)
-                     Last1.add(s.getKey());
-                 }
-    
-    return Last1;
-  }
-}
