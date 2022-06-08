aa1ec8f68eaea116cd170c2406460506ee2b7968
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-06-08 15:30:17 +0530

    Topological Sort :star: :star:

---
+++ Graph/topologicalSort.java
@@ -0,0 +1,36 @@
+static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+        int[] inDegree = new int[V];
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        for(int u=0;u<V;u++)
+        {
+            for(int x : adj.get(u) )
+                inDegree[x]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=0;i<V;i++)
+        {
+            if(inDegree[i] == 0) q.add(i);
+        }
+
+        while( !q.isEmpty() )
+        {
+            int u = q.poll();
+            ans.add(u);
+            
+            for(int x: adj.get(u) )
+            {
+                if(--inDegree[x] == 0 ) q.add(x);
+            }
+        }
+        int res[]=new int[ans.size()];
+        int a=0;
+        for(int i=0;i<ans.size();i++){
+            res[a++]=ans.get(i);
+        }
+        return res;
+    }
