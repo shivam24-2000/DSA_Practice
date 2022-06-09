d029ca64768b5b59bd127eec3659f62ee6c87be4
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-06-09 22:32:17 +0530

    Minimum_Spanning_tree / Prism_algo :star: :star:

--- Graph/MST.java
+++ Graph/MST.java
@@ -1,27 +1,26 @@
 static final int V=4;
-public static int primMST(int graph[][]) 
-{
-  int[] key = new int[V];
-  int res=0;
-  Arrays.fill(key, Integer.MAX_VALUE);
-  
-  boolean[] mSet = new boolean[V];
-  key[0] = 0;
-  
-  for(int count=0;count<V;count++)
-  {
-    int u = -1;
+	public static int primMST(int graph[][]) 
+    { 
     
-    if(!mSet && (u == -1 || key[i] < key[u]) ) u=i;
+    	int[] key=new int[V];int res=0; 
+    	Arrays.fill(key,Integer.MAX_VALUE);
+    	boolean[] mSet=new boolean[V]; key[0]=0;
     
-    mSet[u] = true;
-    res += key[u];
+    	for (int count = 0; count < V ; count++) 
+    	{ 
+    		int u = -1; 
     
-    for(int v=0;v<V;v++)
-    {
-      if( !mSet && graph[u][v] != 0)
-        key[v] = Math.min(key[v], graph[u][v]);
-    }
-  }
-  return res;
-}
+    		for(int i=0;i<V;i++)
+    		    if(!mSet[i]&&(u==-1||key[i]<key[u]))
+    		        u=i;
+    		mSet[u] = true; 
+    		res+=key[u];
+    
+    		
+    		for (int v = 0; v < V; v++) 
+    
+    			if (graph[u][v]!=0 && mSet[v] == false) 
+    				key[v] = Math.min(key[v],graph[u][v]); 
+    	} 
+        return res;
+    } 
