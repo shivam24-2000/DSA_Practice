2381e51f658e95c7e1c61e73ae21b1fc85bd1c53
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-06-10 21:33:10 +0530

    Djikstra. Algo :star:

--- Graph/djikstra.java
+++ Graph/djikstra.java
@@ -1,28 +1,28 @@
-static final int V=4;
-public static int[] djikstra(int graph[][], int src) 
-{
-  int[] dist = new int[V];
-  Arrays.fill(dist, Integer.MAX_VALUE);
-  dist[src] = 0;
-  
-  boolean[] visited = new boolean[V];
-  
-  for(int count = 0; count < V;count++);
-  {
-    int u = -1;
-    
-    for(int i=0;i<V;i++)
+static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int s)
     {
-      if( !visited[i] && ( u == -1 || dist[i] < dist[v])
-         u = i;
-    }
-    visited[u] = true;
+        // Write your code here
+        int[][] graph = new int[V][V];
+        for(int i=0; i<V; i++)
+            for(int j=0; j<adj.get(i).size(); j++)
+                graph[i][adj.get(i).get(j).get(0)] = adj.get(i).get(j).get(1);
+        
+      int[] dist=new int[V];int res=0; 
+    	Arrays.fill(dist,Integer.MAX_VALUE);dist[s]=0;
+    	boolean[] fin=new boolean[V]; 
     
-    for(int v = 0; v < V; v++)
-         {
-           if( graph[u][v] != 0 && !visited[v])
-             dist[v] = Math.min(dist[v], graph[u][v]);
-         }
-  }
-         return dist;
-}
+    	for (int count = 0; count < V-1 ; count++) 
+    	{ 
+    		int u = -1; 
+    
+    		for(int i=0;i<V;i++)
+    		    if(!fin[i]&&(u==-1||dist[i]<dist[u]))
+    		        u=i;
+    		fin[u] = true; 
+    		
+    		for (int v = 0; v < V; v++) 
+    
+    			if (graph[u][v]!=0 && fin[v] == false) 
+    				dist[v] = Math.min(dist[v],dist[u]+graph[u][v]); 
+    	} 
+        return dist;
+    }
