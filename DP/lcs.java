5ef261618a4bbcf4121ef72ed6ab522fa5da3886
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-06-15 14:47:17 +0530

    Longest Common Subsequence :star:

---
+++ DP/lcs.java
@@ -0,0 +1,18 @@
+static int lcs(int x, int y, String s1, String s2)
+    {
+        // your code here
+        int[][] dp = new int[x+1][y+1];
+        
+        for(int i=1; i<=x; i++)
+            {
+                for(int j=1; j<=y; j++)
+                {
+                    if(s1.charAt(i-1)==s2.charAt(j-1))
+                        dp[i][j] = 1 + dp[i-1][j-1];
+                    else
+                        dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        return dp[x][y];
+        
+    }
