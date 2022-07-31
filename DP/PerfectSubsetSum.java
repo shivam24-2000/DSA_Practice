6f6c1cacff285d80d751bfef43329b2e5ce67655
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-07-31 16:15:11 +0530

    Perfect Sum Problem

--- DP/PerfectSubsetSum.java
+++ DP/PerfectSubsetSum.java
@@ -1,26 +1,36 @@
 class Solution{
-    public int SubSet(int n, int[] arr, int sum, int[][] dp)
+    public int SubSetC(int[] arr, int n, int sum, int[][] dp)
     {
-        if(dp[n][sum] != -1) return dp[n][sum];
-        
-        if( sum == 0) return dp[n][sum] = 1;
-        
-        if( arr[n-1] > sum) return dp[n][sum] = SubSet(n-1, arr, sum, dp);
-        else return dp[n][sum] = (SubSet(n-1, arr, sum - arr[n-1], dp) + SubSet(n-1, arr, sum, dp));
+        for(int i=1;i<=n;i++)
+        {
+            for(int j = 0;j<=sum;j++)
+            {
+                if( arr[i-1] > j)
+                {
+                    dp[i][j] = dp[i-1][j]%1000000007;
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j]%1000000007 + dp[i-1][j- arr[i-1]]%1000000007;
+                }
+            }
+        }
+        return (dp[n][sum]%1000000007);
     }
-    
+
 	public int perfectSum(int arr[],int n, int sum) 
 	{ 
 	    // Your code goes here
 	    int[][] dp = new int[n+1][sum+1];
 	    
-	    for(int i=0;i<=n;i++) 
+	    for(int i=0;i<=n;i++)
 	    {
 	        for(int j=0;j<=sum;j++)
 	        {
 	            dp[i][j] = -1;
 	        }
 	    }
+	    
 	    for(int i=0;i<=n;i++)
 	    {
 	        for(int j=0;j<=sum;j++)
@@ -28,8 +38,9 @@ class Solution{
 	            if( i == 0) dp[i][j] = 0;
 	            if( j == 0) dp[i][j] = 1;
 	        }
+	        
 	    }
 	    
-	    return SubSet(n, arr, sum, dp);
+	    return SubSetC(arr, n, sum, dp);
 	} 
 }
