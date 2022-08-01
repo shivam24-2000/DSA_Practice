20812cf693e541b095a38147edcf6af8b5800a9f
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-08-01 22:56:50 +0530

    Knapsack with Duplicate Items

---
+++ DP/RodCutting.java
@@ -0,0 +1,39 @@
+class Solution{
+    public int cutRod(int price[], int N) {
+        //code here
+        int[][] dp = new int[N+1][N+1];
+        
+        for(int i=0;i<=N;i++)
+        {
+            for(int j=0;j<=N;j++)
+            {
+                dp[i][j] = -1;
+            }
+        }
+        
+        for(int i=0;i<=N;i++)
+        {
+            for(int j=0;j<=N;j++)
+            {
+                if(i == 0) dp[i][j] = 0;
+                if(j == 0) dp[i][j] = 0;
+            }
+        }
+        
+        for(int i=1;i<=N;i++)
+        {
+            for(int j=0;j<=N;j++)
+            {
+                if( i > j)
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = Math.max(price[i-1] + dp[i][j-i], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[N][N];
+    }
+}
