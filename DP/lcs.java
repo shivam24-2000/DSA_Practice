64c810ddf22d050c861c08de2d40254570b5d429
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-08-04 11:56:57 +0530

    longest common subsequence

--- DP/lcs.java
+++ DP/lcs.java
@@ -1,3 +1,13 @@
+// recursive method
+static int lcs(int x, int y, String s1, String s2)
+{
+    if( x == 0 || y == 0) return 0;
+
+    if( s1.charAt(x-1) == s2.charAt(y-1)) return 1+ lcs(x-1, y-1, s1, s2);
+    else return Math.max(lcs(x, y-1, s1, s2), lcs(x-1, y, s1, s2))
+}
+
+// dp method
 static int lcs(int x, int y, String s1, String s2)
     {
         // your code here
