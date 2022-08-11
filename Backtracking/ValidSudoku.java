78c6fa5df7c6db0d7250f53b0141eebd592d6438
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-08-11 23:10:54 +0530

    Valid Sudoku

--- Backtracking/ValidSudoku.java
+++ Backtracking/ValidSudoku.java
@@ -7,10 +7,16 @@ class Solution {
         {
             for(int j=0;j<9;j++)
             {
-                if( board[i][j] != '.')
+                if(board[i][j] != '.')
                 {
-                    if(!hs.add("row" + i + board[i][j]) || !hs.add("col" + j + board[i][j]) ) return false;
-                    if(!hs.add("box"+(i/3)*3 + (j/3) + board[i][j])) return false;
+                    if( hs.contains(board[i][j] + "present in" + i +"row") || hs.contains(board[i][j] + "present in" + j + "col") || hs.contains(board[i][j] + "present in" + ((i/3)*3 + (j/3)) + "box")) return false;
+                    else 
+                    {
+                        hs.add(board[i][j]+ "present in" + i + "row");
+                        hs.add(board[i][j]+ "present in" + j + "col");
+                        hs.add(board[i][j]+ "present in" + ((i/3)*3 + (j/3)) + "box");
+                        
+                    }
                 }
             }
         }
