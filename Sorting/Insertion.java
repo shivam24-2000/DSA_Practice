bad0a48c5bf504c8045688f7c4c6084722ad6a31
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-08-12 15:06:03 +0530

    Insertion sort

--- Sorting/Insertion.java
+++ Sorting/Insertion.java
@@ -5,7 +5,7 @@ void insertionSort(int[] arr, int n)
     int key = arr[i];
     int j = i-1;
     
-    while( j>+=0 && arr[j] > key)
+    while( j>=0 && arr[j] > key)
     {
       arr[j+1] = arr[j];
       j--;
