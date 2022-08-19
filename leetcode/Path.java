32d689598cee6c43b60bba61559290f7fefe3bd7
Shivam Singhal <70143573+shivam24-2000@users.noreply.github.com>
2022-08-19 10:13:13 +0530

    Unique Paths

--- leetcode/Path.java
+++ leetcode/Path.java
@@ -1,3 +1,19 @@
+// recursive
+static int numberOfPaths(int m, int n)
+
+    {
+        // If either given row number is first or
+        // given column number is first
+        if (m == 1 || n == 1)
+            return 1;
+ 
+        // If diagonal movements are allowed then
+        // the last addition is required.
+        return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1);
+        // + numberOfPaths(m-1, n-1);
+    }
+
+
 class Solution {
     
     public int uniquePaths(int m, int n) {
